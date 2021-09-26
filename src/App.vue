4a59c9513fd7f00f7107401ec2f5c59549add204
AndresBaez96 <afbaezm@unal.edu.co>
2021-09-26 15:41:13 -0500

    Interfaz nuevo usuario y login en archivo Vue

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+
+    <div>
+      <Login></Login>
+      <NewUser></NewUser>
+    </div>
+   
+    
+</template>
+
+<script>
+import Login from './components/Login.vue'
+import NewUser from './components/NewUser.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    Login,
+    NewUser
+  }
+}
+</script>
+
+<style>
+#app {
+  margin: 0;
+  padding: 0;
+  background: url(./assets/bg3.jpg) no-repeat center top;
+  background-size: cover;
+  font-family: 'Ubuntu', sans-serif;
+  height: 100vh;
+}
+</style>

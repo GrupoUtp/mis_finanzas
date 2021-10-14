c6a0562440964b4c15276ebef2ca61f2cf3d3aa3
Fernando Fernandez <fermen1305@gmail.com>
2021-10-13 20:01:27 -0500

    Funcion agregada: Ingresar egresos a la base mongoDb

--- backend/routes/gasto.js
+++ backend/routes/gasto.js
@@ -1,15 +1,14 @@
 import routerx from 'express-promise-router';
 import gastocontroller from '../controllers/GastoControllers';
-import auth from '../middlewares/auth';
 
 const router = routerx();
 
-router.post('/add',auth.verifyUser,gastocontroller.add);
-router.get('/query',auth.verifyUser,gastocontroller.query);
-router.get('/list',auth.verifyUser,gastocontroller.list);
-router.put('/update',auth.verifyUser,gastocontroller.update);
-router.delete('/remove',auth.verifyUser,gastocontroller.remove);
-router.put('/activate',auth.verifyUser,gastocontroller.activate);
-router.put('/desactivate',auth.verifyUser,gastocontroller.desactivate);
+router.post('/add',gastocontroller.add);
+router.get('/query',gastocontroller.query);
+router.get('/list',gastocontroller.list);
+router.put('/update',gastocontroller.update);
+router.delete('/remove',gastocontroller.remove);
+router.put('/activate',gastocontroller.activate);
+router.put('/desactivate',gastocontroller.desactivate);
 
 export default router;

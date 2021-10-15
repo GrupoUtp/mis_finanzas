48150ba81eb8ca1d46c1738b1263ab124f9254c3
Fernando Fernandez <fermen1305@gmail.com>
2021-10-15 10:46:36 -0500

    Correcion imagen home

--- frontend/src/views/InterfazUsu.vue
+++ frontend/src/views/InterfazUsu.vue
@@ -14,7 +14,7 @@
 
         <div class="row">
           <div class="col-lg-6" data-aos="fade-right" data-aos-delay="100">
-            <img src="@/assets/img/home/ahorro1.jpg" class="img-fluid" alt="">
+            <img src="@/assets/img/home/ahorro1.jpg" class="img-fluid-lading" alt="">
           </div>
           <div class="col-lg-6 pt-4 pt-lg-0 content d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="100">
             <h3>Una billetera organizada</h3>
